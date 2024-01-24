b3aa9b95d01928612a5698a6ebcb78e05d20f434
Aaron Marklund <Aaron@Marklund.name>
2024-01-24 16:16:16 +0100

    Fmod and prototypes

--- Yellow_Team_4/Assets/Script/TestScript.cs
+++ Yellow_Team_4/Assets/Script/TestScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    private float testVariable; // add by brandon
     // Start is called before the first frame update
     void Start()
     {
